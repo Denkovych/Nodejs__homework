e36f7466b41511499ab912fbd166e466bad25be4
Yurii Denkovych <yd2004@ukr.net>
2022-10-23 19:33:52 +0300

    Update app.js

---
+++ controllers/getAll.js
@@ -0,0 +1,25 @@
+const { Contact } = require("../models/contacts");
+
+const getAll = async (req, res, next) => {
+  try {
+    const { _id } = req.user;
+    const { page = 1, limit = 10 } = req.query;
+    const skip = (page - 1) * limit;
+    const contacts = await Contact.find(
+      { owner: _id },
+      "name phone email favorite",
+      { skip, limit: Number(limit) }
+    ).populate("owner", "_id name email");
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        result: contacts,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = getAll;
