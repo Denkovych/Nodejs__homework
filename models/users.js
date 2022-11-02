857da00acbbf800d7cf0d0ed8cd94f6ccc6ed001
Yurii Denkovych <yd2004@ukr.net>
2022-11-02 09:58:57 +0200

    Update users.js

--- models/users.js
+++ models/users.js
@@ -4,27 +4,28 @@ const bcrypt = require("bcryptjs");
 
 
 const userSchema = new Schema(
-{
+  {
     password: {
-        type: String,
-        required: [true, 'Set password for user'],
-  },
+      type: String,
+      required: [true, 'Set password for user'],
+    },
     email: {
-        type: String,
-        required: [true, 'Email is required'],
-        unique: true,
-  },
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+    },
     subscription: {
-        type: String,
-        enum: ["starter", "pro", "business"],
-        default: "starter"
-  },
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter"
+    },
     token: {
-        type: String,
-        default: null
-        }
-}
-)
+      type: String,
+      default: null
+    }
+  },
+  { versionKey: false, timestamps: true }
+);
 
 userSchema.methods.setPassword = function (password) {
   this.password = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
