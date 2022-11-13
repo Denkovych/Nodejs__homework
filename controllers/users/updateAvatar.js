ae5a89b1f98cd03c36a81f41b3a639ab1011653a
Yurii Denkovych <yd2004@ukr.net>
2022-11-13 14:21:28 +0200

    Update updateAvatar.js

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -2,6 +2,8 @@ const { User } = require("../../models");
 const path = require("path");
 const fs = require("fs/promises");
 
+const Jimp = require("jimp");
+
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
@@ -9,7 +11,14 @@ const updateAvatar = async (req, res) => {
   const { _id: id } = req.user;
   const imageName = `${id}_${originalname}`;
   try {
-        const resultUpload = path.join(avatarsDir, imageName);
+      const resultUpload = path.join(avatarsDir, imageName);
+      await Jimp.read(tempUpload)
+          .then((avatar) => {
+              return avatar.resize(250, 250).write(resultUpload);
+          })
+          .catch((err) => {
+              throw err;
+              });
       await fs.rename(tempUpload, resultUpload);
 
     const avatarURL = path.join("public", "avatars", imageName);
